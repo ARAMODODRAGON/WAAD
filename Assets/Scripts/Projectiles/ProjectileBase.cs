f96665e3ddf2861239bc6aea3466aae9aaacc295
MohdAAyyad <mohd.ayyad.maa@gmail.com>
2021-05-21 20:54:35 -0400

    First push

---
+++ Assets/Scripts/Projectiles/ProjectileBase.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBase : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private float speed = 0.0f;
+    private float damage = 0.0f;
+    private Vector2 direction = new Vector2(0.0f, 0.0f);
+    private PlayerController playerRef = null;
+    private bool bHoming = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 movementVec = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y) + direction * speed * Time.deltaTime;
+        rigidBody.MovePosition(movementVec);
+    }
+
+    public void UpdateProjectileParameters(float speed_, float damage_, Vector2 direction_, PlayerController playerRef_ = null, bool bHoming_ = false)
+    {
+
+    }
+}
